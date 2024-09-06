b431b50ff03c26658661c907f9a575541723e8ac
cgp-client <code.gen.pro.system@gmail.com>
2024-09-06 21:09:01 +0300

    Adding db/migrations/001_create_tasks_table.js

---
+++ db/migrations/001_create_tasks_table.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  }
+};
